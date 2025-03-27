1a665a743f1ca8148121c3890f140a4e08f30fc0
Arthur Silva <amaral.psof@gmail.com>
2025-03-27 14:23:09 -0300

    v0.0.1

--- README.md
+++ README.md
@@ -1,31 +1,48 @@
-### _pt-BR:_
-## Detector de Mudanças de Um Botão em Uma Página Web em JavaScript
+## v0.0.1 Changelog
+
+ ### *pt-BR:*
+ - Agora o script roda uma vez ao ser ligado, ao invés de ter que esperar o setTimeout;
+ - Alterei a `const` de `buttonID` para `elementID`, já que o script pode ser utilizado em qualquer elemento `href`;
+ - Agora os logs e alerts estão tanto em português quanto inglês;
+ - Removi a variável botaoSumiu, deixando o código mais limpo.
+
+ ### *en:*
+ - Now the script runs one time when activated, instead of waiting for the setTimeout;
+ - Changed the `const` from `buttonID` to `elementID`, because the script can be used in any `href` element;
+ - Now the logs and alerts are in english and portuguese;
+ - Removed the variable botaoSumiu, leaving the code cleaner.  
+
+
+# Intro:
+
+### *pt-BR:*
+## JavaScript - Detector  por ID de Mudanças de um Elemento `href` em uma Página Web
 
 Eu precisava detectar a mudança de um botão numa página web, então fiz esse script pra isso. Pretendo implementar uma função em node.js que mandará um email ao detectar mudanças, e irá descrevê-las.
 
 ### Como rodar o script?
 
-Em primeiro lugar, acesse o código e mude o valor de ButtonID para o ID do botão (ou elemento) que irá ser detectado na página, depois:
+Em primeiro lugar, acesse o código e mude o valor de `elementID` para o ID do elemento que irá ser detectado na página, depois:
 
 - Copie o código;
 - Vá para a página em que quer rodá-lo;
-- Abra o console (Ctrl + Shift + I pra maioria dos browsers);
+- Abra o console (*Ctrl + Shift + I* para a maioria dos browsers);
 - Cole o código lá e dê enter.
 
 É isso, divirta-se!
 
-### _en:_
-## JavaScript Web Page Button Change Detector
+### *en:*
+## JavaScript - Web Page `href` Element Change Detector by ID
 
 I needed to detect changes from a button in a web page, so I made this script to do that. I plan on implementing a "send email" function, using node.js, that sends an email when a change in the button is detected, describing the change.
 
 ### How to run the script?
 
-First, access the code and change the value of ButtonID to the ID of the Button (or element) that you want to detect changes. Then:
+First, access the code and change the value of `elementID` to the ID of the element that you want to detect changes. Then:
 
 - Copy the script;
 - Go to the webpage you want the script running;
-- Open the console (Ctrl + Shift + I for most browsers);
+- Open the console (*Ctrl + Shift + I* for most browsers);
 - Paste and enter the script.
 
 That's it, have fun!
--- script.js
+++ script.js
@@ -1,38 +1,53 @@
-const buttonID = "ID";
+//Mude "ID" para o ID do elemento que será detectado.
+//Change "ID" to the ID of the element you want to detect.
+const elementID = "ID";
 
-let botaoAntigo = document.getElementById(buttonID);
-let hrefAntigo = botaoAntigo ? botaoAntigo.href : null;
-let textoAntigo = botaoAntigo ? botaoAntigo.innerText : null;
-let botaoSumiu = false;
+let elementoAntigo = document.getElementById(elementID);
+let hrefAntigo = elementoAntigo ? elementoAntigo.href : null;
+let textoAntigo = elementoAntigo ? elementoAntigo.innerText : null;
 
-setInterval(() => {
-  let botao = document.getElementById(buttonID);
+const verificacao = () => {
+  let elemento = document.getElementById(elementID);
+  console.log("Iniciando verificação...\nStarting verification...")
   
-  if (botao) {
-    let href = botao.href;
-    let texto = botao.innerText;
+  if (elemento) {
+    let href = elemento.href;
+    let texto = elemento.innerText;
   
       if (href !== hrefAntigo) {
-        console.log("Mudança detectada no link: " + hrefAntigo + " => " + href);
-        alert("O link do botão mudou!");
+        console.log(
+          "Mudança detectada no link: " + hrefAntigo + " => " + href +
+          "\nChange detected in the link: " + hrefAntigo + " => " + href
+        );
+        alert("O link do elemento mudou!\nThe element link changed!");
         //mandarEmail();
         hrefAntigo = href;
       }
 
       if (texto !== textoAntigo) {
-        console.log("Mudança detectada no texto: " + textoAntigo + " => " + texto);
-        alert("O texto do botão mudou!");
+        console.log(
+          "Mudança detectada no texto: " + textoAntigo + " => " + texto +
+          "\nChange detected in the text: " + textoAntigo + " => " + texto
+        );
+        alert("O texto do elemento mudou!" );
         //mandarEmail();
         textoAntigo = texto;
       }
 
-    } else {
-      if (!botaoSumiu) {
-      alert("O botão sumiu!!");
-      console.log("Botão não encontrado!")
-      botaoSumiu = true;
-      }
-    }
-}, 60*60*1000);
+  } else {
+    console.log(
+      "Elemento não encontrado! ID errado, talvez?" + 
+      "\nElement not found! Wrong ID, perhaps?"
+    )
+    alert("Elemento não encontrado!!\nElement not found!!");
+    clearInterval(verificacao);
+  }
+}
 
 //async function mandarEmail(assunto, mensagem) {}
+
+verificacao();
+
+//Aqui você pode mudar o intervalo de tempo (recomendo a forma 24h*60min*60seg*1000ms), o padrão é 5 segundos.
+//Here you can change the time interval (form 24h*60min*60sec*1000ms recommended), the default is 5 seconds.
+setInterval(verificacao, 5*1000);
